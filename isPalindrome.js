b09081b7750c71e9727af80403e8bb5f2e3a3609
jangwonm <mjw0054@gmail.com>
2023-01-27 23:11:49 +0900

    commit

--- isPalindrome.js
+++ isPalindrome.js
@@ -5,10 +5,10 @@
 
 let x1 = 121;
 let x2 = -121;
+
 var isPalindrome = function(x) {
   let temp1 = '';
   let temp2 = '';
-  console.log(x.toString());
   temp1 = x.toString();
   for (let i = temp1.length; i > 0; i--) {
     temp2 += temp1[i-1];
@@ -20,5 +20,5 @@ var isPalindrome = function(x) {
   }
 };
 
-isPalindrome(x1);
-isPalindrome(x2);
+console.log(isPalindrome(x1));
+console.log(isPalindrome(x2));
